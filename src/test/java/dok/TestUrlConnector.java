c9861c14c1bbd2187dc0277134e57abdec10175b
do.kim <do.kim@DO-OFFICE-LT>
2019-08-28 10:27:25 +0900

    로그 정리

--- src/test/java/dok/TestUrlConnector.java
+++ src/test/java/dok/TestUrlConnector.java
@@ -10,71 +10,79 @@ import com.dok.common.support.UrlConnector;
 
 public class TestUrlConnector {
 
-  class Caller implements Callable<String> {
+    class Caller implements Callable<String> {
 
-    private String       name        = null;
-    private int          actionCount = 1000;
-    private long         sleep       = 1000;
+        private String       name;
+        private int          actionCount;
+        private long         sleep;
 
-    private UrlConnector urlc        = null;
+        private UrlConnector urlc;
 
 
-    public Caller(String name, String query, int actionCount) {
-      this.name = name;
-      this.actionCount = actionCount;
+        public Caller(String name, int actionCount, long sleep, String query) {
+            this.name = name;
+            this.actionCount = actionCount;
+            this.sleep = sleep;
 
-      this.urlc = UrlConnector.configure()
-                              .url("https://search.naver.com/search.naver")
-                              .param("sm", "top_hty")
-                              .param("fbm", "0")
-                              .param("ie", "utf8")
-                              .param("query", query)
-                              .cookie(true)
-                              .build();
-    }
-
-    @Override
-    public String call() throws Exception {
-      Counter count = new Counter();
-      ExecutorService executor = Executors.newCachedThreadPool();
-      Runnable runner = () -> {
-        for (int j = 0; j < actionCount; j++) {
-          if(sleep > 0) {
-            try { Thread.sleep(sleep); } catch(Exception ex) { }
-          }
-          new Thread(() -> {
-              String s = urlc.get();
-            System.out.printf("> %04d - %s %n ", count.increment(), s == null ? "NULL" : s.length() > 100 ? s.substring(0, 100): s);
-          }).start();
+              this.urlc = UrlConnector.configure()
+                                      .url("https://search.naver.com/search.naver")
+                                      .param("sm", "top_hty")
+                                      .param("fbm", "0")
+                                      .param("ie", "utf8")
+                                      .param("query", query)
+                                      .cookie(true)
+                                      .build();
         }
-      };
-      executor.execute(runner);
-      executor.shutdown();
-      return name + " " + count.value();
-    }
 
-    public class Counter {
-        private int value = 0;
-        public synchronized int increment() {
-            return ++value;
+        @Override
+        public String call() throws Exception {
+            Counter count = new Counter();
+            ExecutorService executor = Executors.newCachedThreadPool();
+            Runnable runner = () -> {
+                for (int j = 0; j < actionCount; j++) {
+                    if(sleep > 0) {
+                        try {
+                            Thread.sleep(sleep);
+                        } catch(Exception ex) {
+                        }
+                    }
+                    new Thread(() -> {
+                        String res = urlc.get();
+                        String str = (res == null ? "NULL" : (res.length() > 100 ? res.substring(0, 100) : res));
+                        System.out.printf("> %s-%04d  %s %n", name, count.increment(), str);
+                    }).start();
+                }
+            };
+            executor.execute(runner);
+            executor.shutdown();
+            return name + " " + count.value();
         }
-        public int value() {
-            return value;
+
+
+        public class Counter {
+
+            private int value = 0;
+
+
+            public synchronized int increment() {
+                return ++value;
+            }
+
+            public int value() {
+                return value;
+            }
         }
-    }
 
   }
 
 
-  public void execute(String query, int actionCount) {
+  public void execute(int taskCount, int actionCount, long sleep, String query) {
       System.out.println("Init...");
-      int taskCount = Runtime.getRuntime().availableProcessors();
-      int sleep = 1000;
       System.out.printf(">> tasks=%d, actions=%d, sleep=%d%n", taskCount, actionCount, sleep);
 
       List<Callable<String>> tasks = new ArrayList<>();
       for(int i = 0; i < taskCount; i++) {
-          Caller caller = new Caller(String.format("Task%04d", i+1), query, actionCount);
+          Caller caller = new Caller(String.format("Task%04d", i+1), actionCount, sleep, query);
           //caller.init();
           tasks.add(caller);
       }
@@ -91,18 +99,17 @@ public class TestUrlConnector {
   }
 
   public static void main(String[] args) {
-    String query;
-    int actionCount;
-      if(args.length == 2) {
-        query = (args[0] == null || args[0].isEmpty()) ? "" : args[0];
-        actionCount = (args[1] == null || args[1].isEmpty()) ? 10 : Integer.parseInt(args[1]);
-      } else {
-        query = "조국힘내세요";
-        actionCount = 1000;
-      }
+      int taskCount = Runtime.getRuntime().availableProcessors();
+      int actionCount = 100;
+      long sleep = 10000L;
+      String query = "\uC870\uAD6D\uD798\uB0B4\uC138\uC694";
+//      if(args.length == 2) {
+//        query = (args[0] == null || args[0].isEmpty()) ? "" : args[0];
+//        actionCount = (args[1] == null || args[1].isEmpty()) ? 10 : Integer.parseInt(args[1]);
+//      }
 
       TestUrlConnector test = new TestUrlConnector();
-      test.execute(query, actionCount);
+      test.execute(taskCount, actionCount, sleep, query);
   }
 
 
