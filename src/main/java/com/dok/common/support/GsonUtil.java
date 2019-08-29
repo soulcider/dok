cfc73692ac0204139bc744f4a10501377aaeed09
kim.do <kim.do@LCNC13V0428.corp.lgcns.com>
2019-08-29 15:33:48 +0900

    xml2json

--- src/main/java/com/dok/common/support/GsonUtil.java
+++ src/main/java/com/dok/common/support/GsonUtil.java
@@ -3,7 +3,6 @@ package com.dok.common.support;
 
 import com.dok.common.helper.DokObjectDeserializer;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -21,16 +20,16 @@ public class GsonUtil {
     if(json == null || json.isEmpty()) {
       return xml;
     } else {
-      TypeReference<Object> typeRef = new TypeReference<Object>(){};
+      //TypeReference<Object> typeRef = new TypeReference<Object>(){};
       try {
         ObjectMapper mapper = new ObjectMapper();
         mapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        Object object = mapper.readValue(json, typeRef);
+        Object object = mapper.readValue(json, Object.class);
         //System.out.println(map);
 
         XmlMapper xmlMapper = new XmlMapper();
-        xmlMapper.setSerializationInclusion(Include.NON_NULL);
+        xmlMapper.setSerializationInclusion(Include.ALWAYS);
         xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
         xml = xmlMapper.writeValueAsString(object);
       } catch(Exception ex) {
@@ -45,19 +44,22 @@ public class GsonUtil {
     if(xml == null || xml.isEmpty()) {
       return json;
     } else {
-      TypeReference<Object> typeRef = new TypeReference<Object>(){};
+      //TypeReference<Object> typeRef = new TypeReference<Object>(){};
       try {
         //XmlMapper xmlMapper = new XmlMapper();
         //xmlMapper.setSerializationInclusion(Include.NON_NULL);
         DokObjectDeserializer uod = new DokObjectDeserializer();
         SimpleModule module = new SimpleModule().addDeserializer(Object.class, uod);
         XmlMapper xmlMapper = (XmlMapper) new XmlMapper().registerModule(module);
-        Object object = xmlMapper.readValue(xml, typeRef);
+        Object object = xmlMapper.readValue(xml, Object.class);
         //System.out.println(object);
 
         ObjectMapper mapper = new ObjectMapper();
-        mapper.setSerializationInclusion(Include.NON_NULL);
+        mapper.setSerializationInclusion(Include.ALWAYS);
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        //if(Object.class.getAnnotation(JsonRootName.class) != null) {
+        //  mapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
+        //}
         json = mapper.writeValueAsString(object);
       } catch(Exception ex) {
         //System.err.println(ex.getMessage());
